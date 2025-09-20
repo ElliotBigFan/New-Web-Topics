17e6fe4aa05a114a6357100d1393b10683ca5a54
ElliotBigFan <lengocthuanti21@gmail.com>
2025-09-20 12:35:55 +0700

    upload file race condition

--- Upload-file-race-condition/www/validator.php
+++ Upload-file-race-condition/www/validator.php
@@ -1,7 +1,8 @@
 <?php
 function validate_and_maybe_delete($path) {
     // Giả lập thời gian quét/AV — tăng lên để dễ thử nghiệm race
-    sleep(5); // <-- Tăng cửa sổ race (thử thay bằng 10 nếu muốn)
+    //sleep(5); // <-- Tăng cửa sổ race (thử thay bằng 10 nếu muốn)
+    sleep(0.8);
     $allowed = ['gif', 'jpeg', 'jpg', 'png'];
     $ext = @end(explode('.', $path));
     if (!@in_array($ext, $allowed)) {
